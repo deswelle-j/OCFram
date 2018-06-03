f9c592ad8a0299910a711b7c302709327e42b0bf
deswelle-j <deswelle.johan@gmail.com>
2018-06-03 21:46:28 +0200

    add router files

---
+++ lib/OCFram/Router.php
@@ -0,0 +1,51 @@
+<?php
+namespace OCFram;
+
+class Router
+{
+    protected $routes = [];
+
+    const NO_ROUTE = 1;
+
+    public function addRoute(Route $route)
+    {
+        if( !in_array($route, $this->routes))
+        {
+            $this->routes[] = $route;
+        }
+    }
+
+    public function getRoute($url)
+    {
+        foreach($this->routes as $route)
+        {
+            //Si la route correspond à l'URL
+            if (($varsValues = $route->match($url)) !== false)
+            {
+                // Si elle a des variables
+                if ($route->hasVars())
+                {
+                    $varsNames = $route->varsNames();
+                    $listVars = [];
+
+                    // On crée n nouveau tableau clé/valeur
+                    // (clé = nom de la variable, valeur = sa valeur)
+                    foreach($varsValues as $key => $match)
+                    {
+                        // La première valeur contient entièrement la chaine capturée
+                        if ($key !== 0)
+                        {
+                            $listVars[$varsNames[$key -1]] = $match;
+                        }
+                    }
+
+                    // On assigne ce tableau de variable a la route
+                    $route->setVars($listVars);
+                }
+
+                return $route;
+            }
+        }
+        throw new \RuntimeException('Aucune route ne correspond à l\'URL', self::NO_ROUTE);
+    }
+}
