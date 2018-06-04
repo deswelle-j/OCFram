cf2cb055e890e55c22b26a5473a3e4942656fac6
deswelle-j <deswelle.johan@gmail.com>
2018-06-04 13:38:27 +0200

    getController() function in Application

--- lib/OCFram/Application.php
+++ lib/OCFram/Application.php
@@ -8,9 +8,49 @@ abstract class Application
     protected $name;
 
     public function __construct(){
-        $this->httpRequest = new HTTPRequest;
-        $this->httpResponse = new HTTPResponse;
+        $this->httpRequest = new HTTPRequest($this);
+        $this->httpResponse = new HTTPResponse($this);
         $this->name = '';
+    }
+
+    public function getController()
+    {
+        $router = new Router;
+
+        $xml = new \DOMDocument;
+        $xml->load(__DIR__.'/../App/'.$this->name.'/Config/routes.xml');
+        $routes = $xml->getElementsByTagName('route');
+        //On parcourt les routes du fichier XML.
+        foreach($routes as $route)
+        {
+            $var = [];
+            // On regarde si des variables sont présentes dans l'URL.
+            if($route->hasAttribute('vars'))
+            {
+                $var = explode(',', $route->getAttribute('vars'));
+            }
+            // On ajoute la route au routeur.
+            $router->addRoute(new Route($route->getAttribute('url'), $route->getAttribute('module'), $route->getAttribute('action'), $vars));
+        }
+        try
+        {
+            // On récupère la route correspondante à l'URL.
+            $matchedRoute = $router->getRoute($this->httpRequest->requestURI());
+        }
+        catch (\RuntimeException $e)
+        {
+            if($e->getCode() == Router::NO_ROUTE)
+            {
+                // Si aucune route ne correspond, c'est que la page demandée n'éxiste pas.
+                $this->httpResponse->redirect404();
+            }
+        }
+        // On ajoute les variables de l'URL au tableau $_GET.
+        $_GET = array_merge($_GET, $matchedRoute->vars());
+
+        // On instancie le contrôleur.
+        $controllerclass = 'App\\'.$this->name.'\\Modules\\'.$matchedRoute->module().'\\'.$matchedRoute->module().'Controller';
+        return new $controllerClass($this, $matchedRoute->module(), $matchedRoute->action());
     }
 
     abstract public function run();
